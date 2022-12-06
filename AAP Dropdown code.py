0728bebb8f03354414de6f0ad245aed4ace99e5c
bhiggin9 <110861488+bhiggin9@users.noreply.github.com>
2022-12-05 23:05:47 -0500

    12/5
    edits

--- AAP Dropdown code.py
+++ AAP Dropdown code.py
@@ -2,7 +2,7 @@ from tkinter import *
  
 root=Tk() 
  
- #Define the size of window or frame
+ #Define the size of window or frame of application and text box
 root.geometry( "300x300" )
 
 #Set the Menu initially
@@ -14,6 +14,8 @@ drop= OptionMenu(root, menu,"Long-term control" , "Caution" , "Quick relief resc
 drop.pack()
 menu= StringVar()
 
+#Returning a value from a function that is execueted from the Tkinter button
+#Defining method to button 2
 def method2(): 
     x="Flovent HFA Inhaler 2 puffs twice daily \nAdvair 45/21 2 puffs twice daily \nPulmicort Inhaler 2 puffs twice daily \nMontelukast 4mg once daily \nSingular 4mg once daily" 
     m.set(x) 
@@ -22,6 +24,7 @@ m=StringVar()
 b1=Button(root, text="long term", command=method2).pack() 
 lb1=Label(root, text="...", textvariable=m).pack() 
 
+#Defining method to button 3
 def method3(): 
     x="Albuterol Inhaler 2 puffs every 4 hrs as needed \nFlovent HFA Inhaler 2 puffs twice daily \nAdvair 45/21 2 puffs twice daily" 
     m.set(x) 
@@ -30,6 +33,7 @@ m=StringVar()
 b2=Button(root, text="caution", command=method3).pack() 
 lb2=Label(root, text="...", textvariable=m).pack() 
  
+ #Defining method to button 4
 def method4(): 
     x="Albuterol inhaler 4 puffs every 20 mins for 40 mins \nVentolin HFA 4 puffs every 20 min for 40 mins \nXopenox 2 puffs every 20 mins for 40 mins \nProventil 2 puffs every 20 mins for 40 mins \nProAir 2 puffs every 20 mins for 40 mins" 
     m.set(x) 
@@ -38,6 +42,7 @@ m=StringVar()
 b2=Button(root, text="quick relief", command=method4).pack() 
 lb2=Label(root, text="...", textvariable=m).pack() 
 
+#Defining method to button 5
 def method5(): 
     x="Claritin 2.5-5mg daily \nZyrtec 2.5-5mg daily" 
     m.set(x) 
